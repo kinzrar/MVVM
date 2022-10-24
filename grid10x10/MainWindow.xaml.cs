060e8d022052fefa321733cb9e09bcbb83451efb
Дима Чернявский <dimasikok-2003@mail.ru>
2022-10-24 13:06:38 -0700

    Добавьте файлы проекта.

---
+++ grid10x10/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace grid10x10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateGrid(10, 10);
+        }
+
+        private void CreateGrid(int x, int y)
+        {
+            var grid = new Grid();
+
+            // Row Definition
+            for (int i = 0; i < x; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            // Column Definition
+            for (int i = 0; i < y; i++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            // Add buttons
+            int n = 1;
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    var button = new Button();
+                    button.Content = n.ToString();
+                    button.Name = "Button" + n.ToString();
+                    button.Click += Button_Click;
+
+
+                    Grid.SetRow(button, i);
+                    Grid.SetColumn(button, j);
+
+                    grid.Children.Add(button);
+                    n++;
+                }
+            }
+            this.Content = grid;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var b = sender as Button;
+            if (b == null)
+            {
+                return;
+            }
+            MessageBox.Show(b.Content.ToString());
+        }
+    }
+}
